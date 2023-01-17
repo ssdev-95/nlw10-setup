7312512170e079c7de43693ffffb8a2e78476da4
SalomaoS <souza95salomao@gmail.com>
2023-01-17 03:52:04 +0000

    bootstrap web application

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+export function App() {
+  return (
+	  <div className="min-h-screen w-screen flex items-center justify-center text-[#f0f2f5f]">
+			<h1>
+			  Hello nlw..
+			</h1>
+		</div>
+	)
+}
