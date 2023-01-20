8ec6380cf3697a12d5fa5a166443210b45224b86
SalomaoS <souza95salomao@gmail.com>
2023-01-20 00:08:50 +0000

    update: refactoring db connection

--- src/infra/typeorm/entity/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './Day'
-export * from './DayHabit'
-export * from './Habit'
-export * from './HabitWeekDays'
--- src/infra/typeorm/repositories.ts
+++
@@ -1,8 +0,0 @@
-import { AppDataSource } from './data-source'
-import { Habit } from './entity/Habit'
-import { HabitWeekDays } from './entity/HabitWeekDays'
-
-const { getRepository } = AppDataSource
-
-export const habitsRepository = getRepository(Habit)
-export const weekDaysRepository = getRepository(HabitWeekDays)
